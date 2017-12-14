609662363dc6c902a9e8327f96049acbf528c94e
melaniemkwon <melaniemkwon@gmail.com>
2017-12-14 14:10:38 -0800

    Components Part1

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -1,16 +1,35 @@
 import { BrowserModule } from '@angular/platform-browser';
+import { BrowserAnimationsModule } from '@angular/platform-browser/animations';
 import { NgModule } from '@angular/core';
+import { 
+  MatButtonModule,
+  MatToolbarModule, 
+  MatListModule
+} from '@angular/material'; 
+import { FlexLayoutModule } from '@angular/flex-layout';
+// import { FormsModule } from '@angular/forms';
+// import { HttpModule } from '@angular/http';
 
+import 'hammerjs';
 
 import { AppComponent } from './app.component';
+import { MenuComponent } from './menu/menu.component';
 
 
 @NgModule({
   declarations: [
-    AppComponent
+    AppComponent,
+    MenuComponent
   ],
   imports: [
-    BrowserModule
+    BrowserModule, 
+    BrowserAnimationsModule,
+    MatButtonModule,
+    MatToolbarModule,
+    MatListModule,
+    FlexLayoutModule
+    // FormsModule,
+    // HttpModule,
   ],
   providers: [],
   bootstrap: [AppComponent]
